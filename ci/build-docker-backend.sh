df54f94cf5271c44616cf815de3226aa327cf841
jonasslarsen <larsengfx1707@gmail.com>
2024-08-15 09:28:33 +0000

    fix: Replace app/ with backend/ or frontend/

--- ci/build-docker-backend.sh
+++ ci/build-docker-backend.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
-cd backend
 REPO="ghcr.io/$registry_name/"
 echo "${REPO}"
-docker build -t "${REPO}go-app:latest" -t "${REPO}go-app:1.0-$Tag" app/
+docker build -t "${REPO}go-app:latest" -t "${REPO}go-app:1.0-$Tag" backend/
