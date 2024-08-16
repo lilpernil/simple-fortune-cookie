1c8ebdf5580b8447c46c47b67025cc3ecfa2ad9e
Kasper Møller <kthom22@student.sdu.dk>
2024-08-16 18:57:26 +0000

    Fault injection test
    Co-authored-by: lilpernil <pernh20@student.sdu.dk>

--- test/frontend-availability-test.sh
+++ test/frontend-availability-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s http://localhost:8080 | grep -q "Fortune cookie"
+curl -s http://localhost:8080 | grep -q "Fortune cake"
 
 if [ $? -eq 0 ]; then
     echo "Frontend available"
